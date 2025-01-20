ef25634b9fd0a10046a2126bab4f1a2ce5484afe
Rafael Belle <rafa.belle@hotmail.com>
2025-01-19 22:32:50 -0300

    added automapper

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -1,16 +1,22 @@
-﻿using CashFlow.Application.UseCases.Expenses.Register;
-using CashFlow.Domain.Repositories.Expenses;
+﻿using CashFlow.Application.AutoMapper;
+using CashFlow.Application.UseCases.Expenses.Register;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CashFlow.Application;
 public static class DependencyInjectionExtension
 {
     public static void AddApplication(this IServiceCollection services)
+    {
+        AddAutoMapper(services);
+        AddUseCases(services);
+    }
+
+    private static void AddAutoMapper(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(AutoMapping));
+    }
+
+    private static void AddUseCases(IServiceCollection services)
     {
         services.AddScoped<IRegisterExpenseUseCase, RegisterExpenseUseCase>();
     }
